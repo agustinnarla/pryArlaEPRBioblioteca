0b51d286b718d527206c0a552a78143bd2d11ae2
Agustin Arla <agusa@DESKTOP-0BG6L2K>
2022-09-18 19:30:50 -0300

    Creacion grilla y adjunte de datos

--- frmBiblioteca.Designer.cs
+++ frmBiblioteca.Designer.cs
@@ -32,29 +32,41 @@
             this.cmdConsultar = new System.Windows.Forms.Button();
             this.cmdAtras = new System.Windows.Forms.Button();
             this.cmdAdelante = new System.Windows.Forms.Button();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre_Libro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Codigo_Editorial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Codigo_Autor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Codigo_Distribuidor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.grlLibros)).BeginInit();
             this.SuspendLayout();
             // 
             // grlLibros
             // 
             this.grlLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grlLibros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre_Libro,
+            this.Codigo_Editorial,
+            this.Codigo_Autor,
+            this.Codigo_Distribuidor});
             this.grlLibros.Location = new System.Drawing.Point(12, 12);
             this.grlLibros.Name = "grlLibros";
-            this.grlLibros.Size = new System.Drawing.Size(753, 109);
+            this.grlLibros.Size = new System.Drawing.Size(663, 85);
             this.grlLibros.TabIndex = 0;
             // 
             // cmdConsultar
             // 
-            this.cmdConsultar.Location = new System.Drawing.Point(370, 127);
+            this.cmdConsultar.Location = new System.Drawing.Point(294, 103);
             this.cmdConsultar.Name = "cmdConsultar";
             this.cmdConsultar.Size = new System.Drawing.Size(75, 23);
             this.cmdConsultar.TabIndex = 1;
             this.cmdConsultar.Text = "Consultar";
             this.cmdConsultar.UseVisualStyleBackColor = true;
+            this.cmdConsultar.Click += new System.EventHandler(this.cmdConsultar_Click);
             // 
             // cmdAtras
             // 
-            this.cmdAtras.Location = new System.Drawing.Point(126, 127);
+            this.cmdAtras.Location = new System.Drawing.Point(50, 103);
             this.cmdAtras.Name = "cmdAtras";
             this.cmdAtras.Size = new System.Drawing.Size(75, 23);
             this.cmdAtras.TabIndex = 2;
@@ -63,18 +75,45 @@
             // 
             // cmdAdelante
             // 
-            this.cmdAdelante.Location = new System.Drawing.Point(614, 127);
+            this.cmdAdelante.Location = new System.Drawing.Point(538, 103);
             this.cmdAdelante.Name = "cmdAdelante";
             this.cmdAdelante.Size = new System.Drawing.Size(75, 23);
             this.cmdAdelante.TabIndex = 3;
             this.cmdAdelante.Text = " >  > ";
             this.cmdAdelante.UseVisualStyleBackColor = true;
             // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Width = 50;
+            // 
+            // Nombre_Libro
+            // 
+            this.Nombre_Libro.HeaderText = "Nombre Libro";
+            this.Nombre_Libro.Name = "Nombre_Libro";
+            this.Nombre_Libro.Width = 250;
+            // 
+            // Codigo_Editorial
+            // 
+            this.Codigo_Editorial.HeaderText = "Codigo Editorial";
+            this.Codigo_Editorial.Name = "Codigo_Editorial";
+            // 
+            // Codigo_Autor
+            // 
+            this.Codigo_Autor.HeaderText = "Codigo Autor";
+            this.Codigo_Autor.Name = "Codigo_Autor";
+            // 
+            // Codigo_Distribuidor
+            // 
+            this.Codigo_Distribuidor.HeaderText = "Codigo Distribuidor";
+            this.Codigo_Distribuidor.Name = "Codigo_Distribuidor";
+            // 
             // frmBiblioteca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 169);
+            this.ClientSize = new System.Drawing.Size(696, 150);
             this.Controls.Add(this.cmdAdelante);
             this.Controls.Add(this.cmdAtras);
             this.Controls.Add(this.cmdConsultar);
@@ -92,6 +131,11 @@
         private System.Windows.Forms.Button cmdConsultar;
         private System.Windows.Forms.Button cmdAtras;
         private System.Windows.Forms.Button cmdAdelante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre_Libro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo_Editorial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo_Autor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo_Distribuidor;
     }
 }
 
--- frmBiblioteca.cs
+++ frmBiblioteca.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace pryArlaEPRBioblioteca
 {
@@ -15,6 +17,23 @@ namespace pryArlaEPRBioblioteca
         public frmBiblioteca()
         {
             InitializeComponent();
+        }
+
+        private void cmdConsultar_Click(object sender, EventArgs e)
+        {
+
+            char separador = Convert.ToChar(",");
+        
+            StreamReader srLibros = new StreamReader("./LIBRO.txt");
+            
+            while (!srLibros.EndOfStream)
+            {
+                string[] vecBiblioteca = srLibros.ReadLine().Split(separador);
+                grlLibros.Rows.Add(vecBiblioteca);
+            
+            }
+            srLibros.Close();
+
         }
     }
 }
