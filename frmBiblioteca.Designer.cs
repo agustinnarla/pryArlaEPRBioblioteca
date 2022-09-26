fbe95664681c22e062da784c62362d46b4dd276f
Agustin Arla <agusa@DESKTOP-0BG6L2K>
2022-09-26 16:15:17 -0300

    Terminado

--- frmBiblioteca.Designer.cs
+++ frmBiblioteca.Designer.cs
@@ -28,114 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.grlLibros = new System.Windows.Forms.DataGridView();
-            this.cmdConsultar = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBiblioteca));
             this.cmdAtras = new System.Windows.Forms.Button();
             this.cmdAdelante = new System.Windows.Forms.Button();
-            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Nombre_Libro = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Codigo_Editorial = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Codigo_Autor = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Codigo_Distribuidor = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.grlLibros)).BeginInit();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre_Libro = new System.Windows.Forms.Label();
+            this.lblCodigo_Autor = new System.Windows.Forms.Label();
+            this.lblNombre_Editorial = new System.Windows.Forms.Label();
+            this.lblNombre_Distribuidor = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombreLibro = new System.Windows.Forms.TextBox();
+            this.txtCodigoAutor = new System.Windows.Forms.TextBox();
+            this.txtNombreEdit = new System.Windows.Forms.TextBox();
+            this.txtNombreDistribuidor = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // grlLibros
-            // 
-            this.grlLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grlLibros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Codigo,
-            this.Nombre_Libro,
-            this.Codigo_Editorial,
-            this.Codigo_Autor,
-            this.Codigo_Distribuidor});
-            this.grlLibros.Location = new System.Drawing.Point(12, 12);
-            this.grlLibros.Name = "grlLibros";
-            this.grlLibros.Size = new System.Drawing.Size(663, 85);
-            this.grlLibros.TabIndex = 0;
-            // 
-            // cmdConsultar
-            // 
-            this.cmdConsultar.Location = new System.Drawing.Point(294, 103);
-            this.cmdConsultar.Name = "cmdConsultar";
-            this.cmdConsultar.Size = new System.Drawing.Size(75, 23);
-            this.cmdConsultar.TabIndex = 1;
-            this.cmdConsultar.Text = "Consultar";
-            this.cmdConsultar.UseVisualStyleBackColor = true;
-            this.cmdConsultar.Click += new System.EventHandler(this.cmdConsultar_Click);
             // 
             // cmdAtras
             // 
-            this.cmdAtras.Location = new System.Drawing.Point(50, 103);
+            this.cmdAtras.Location = new System.Drawing.Point(149, 174);
             this.cmdAtras.Name = "cmdAtras";
             this.cmdAtras.Size = new System.Drawing.Size(75, 23);
             this.cmdAtras.TabIndex = 2;
             this.cmdAtras.Text = "<<";
             this.cmdAtras.UseVisualStyleBackColor = true;
+            this.cmdAtras.Click += new System.EventHandler(this.cmdAtras_Click);
             // 
             // cmdAdelante
             // 
-            this.cmdAdelante.Location = new System.Drawing.Point(538, 103);
+            this.cmdAdelante.Location = new System.Drawing.Point(235, 174);
             this.cmdAdelante.Name = "cmdAdelante";
             this.cmdAdelante.Size = new System.Drawing.Size(75, 23);
             this.cmdAdelante.TabIndex = 3;
             this.cmdAdelante.Text = " >  > ";
             this.cmdAdelante.UseVisualStyleBackColor = true;
+            this.cmdAdelante.Click += new System.EventHandler(this.cmdAdelante_Click);
             // 
-            // Codigo
+            // lblCodigo
             // 
-            this.Codigo.HeaderText = "Codigo";
-            this.Codigo.Name = "Codigo";
-            this.Codigo.Width = 50;
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(9, 22);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 4;
+            this.lblCodigo.Text = "Código ";
             // 
-            // Nombre_Libro
+            // lblNombre_Libro
             // 
-            this.Nombre_Libro.HeaderText = "Nombre Libro";
-            this.Nombre_Libro.Name = "Nombre_Libro";
-            this.Nombre_Libro.Width = 250;
+            this.lblNombre_Libro.AutoSize = true;
+            this.lblNombre_Libro.Location = new System.Drawing.Point(9, 51);
+            this.lblNombre_Libro.Name = "lblNombre_Libro";
+            this.lblNombre_Libro.Size = new System.Drawing.Size(87, 13);
+            this.lblNombre_Libro.TabIndex = 5;
+            this.lblNombre_Libro.Text = "Nombre del Libro";
             // 
-            // Codigo_Editorial
+            // lblCodigo_Autor
             // 
-            this.Codigo_Editorial.HeaderText = "Codigo Editorial";
-            this.Codigo_Editorial.Name = "Codigo_Editorial";
+            this.lblCodigo_Autor.AutoSize = true;
+            this.lblCodigo_Autor.Location = new System.Drawing.Point(9, 80);
+            this.lblCodigo_Autor.Name = "lblCodigo_Autor";
+            this.lblCodigo_Autor.Size = new System.Drawing.Size(85, 13);
+            this.lblCodigo_Autor.TabIndex = 6;
+            this.lblCodigo_Autor.Text = "Código del Autor";
             // 
-            // Codigo_Autor
+            // lblNombre_Editorial
             // 
-            this.Codigo_Autor.HeaderText = "Codigo Autor";
-            this.Codigo_Autor.Name = "Codigo_Autor";
+            this.lblNombre_Editorial.AutoSize = true;
+            this.lblNombre_Editorial.Location = new System.Drawing.Point(9, 109);
+            this.lblNombre_Editorial.Name = "lblNombre_Editorial";
+            this.lblNombre_Editorial.Size = new System.Drawing.Size(110, 13);
+            this.lblNombre_Editorial.TabIndex = 7;
+            this.lblNombre_Editorial.Text = "Nombre de la Editorial";
             // 
-            // Codigo_Distribuidor
+            // lblNombre_Distribuidor
             // 
-            this.Codigo_Distribuidor.HeaderText = "Codigo Distribuidor";
-            this.Codigo_Distribuidor.Name = "Codigo_Distribuidor";
+            this.lblNombre_Distribuidor.AutoSize = true;
+            this.lblNombre_Distribuidor.Location = new System.Drawing.Point(9, 138);
+            this.lblNombre_Distribuidor.Name = "lblNombre_Distribuidor";
+            this.lblNombre_Distribuidor.Size = new System.Drawing.Size(116, 13);
+            this.lblNombre_Distribuidor.TabIndex = 8;
+            this.lblNombre_Distribuidor.Text = "Nombre del Distribuidor";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(149, 15);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.ReadOnly = true;
+            this.txtCodigo.Size = new System.Drawing.Size(74, 20);
+            this.txtCodigo.TabIndex = 9;
+            // 
+            // txtNombreLibro
+            // 
+            this.txtNombreLibro.Location = new System.Drawing.Point(149, 45);
+            this.txtNombreLibro.Name = "txtNombreLibro";
+            this.txtNombreLibro.ReadOnly = true;
+            this.txtNombreLibro.Size = new System.Drawing.Size(161, 20);
+            this.txtNombreLibro.TabIndex = 10;
+            // 
+            // txtCodigoAutor
+            // 
+            this.txtCodigoAutor.Location = new System.Drawing.Point(149, 75);
+            this.txtCodigoAutor.Name = "txtCodigoAutor";
+            this.txtCodigoAutor.ReadOnly = true;
+            this.txtCodigoAutor.Size = new System.Drawing.Size(74, 20);
+            this.txtCodigoAutor.TabIndex = 11;
+            // 
+            // txtNombreEdit
+            // 
+            this.txtNombreEdit.Location = new System.Drawing.Point(149, 105);
+            this.txtNombreEdit.Name = "txtNombreEdit";
+            this.txtNombreEdit.ReadOnly = true;
+            this.txtNombreEdit.Size = new System.Drawing.Size(161, 20);
+            this.txtNombreEdit.TabIndex = 12;
+            // 
+            // txtNombreDistribuidor
+            // 
+            this.txtNombreDistribuidor.Location = new System.Drawing.Point(149, 135);
+            this.txtNombreDistribuidor.Name = "txtNombreDistribuidor";
+            this.txtNombreDistribuidor.ReadOnly = true;
+            this.txtNombreDistribuidor.Size = new System.Drawing.Size(161, 20);
+            this.txtNombreDistribuidor.TabIndex = 13;
             // 
             // frmBiblioteca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 150);
+            this.ClientSize = new System.Drawing.Size(339, 204);
+            this.Controls.Add(this.txtNombreDistribuidor);
+            this.Controls.Add(this.txtNombreEdit);
+            this.Controls.Add(this.txtCodigoAutor);
+            this.Controls.Add(this.txtNombreLibro);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblNombre_Distribuidor);
+            this.Controls.Add(this.lblNombre_Editorial);
+            this.Controls.Add(this.lblCodigo_Autor);
+            this.Controls.Add(this.lblNombre_Libro);
+            this.Controls.Add(this.lblCodigo);
             this.Controls.Add(this.cmdAdelante);
             this.Controls.Add(this.cmdAtras);
-            this.Controls.Add(this.cmdConsultar);
-            this.Controls.Add(this.grlLibros);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmBiblioteca";
             this.Text = "Biblioteca";
-            ((System.ComponentModel.ISupportInitialize)(this.grlLibros)).EndInit();
+            this.Load += new System.EventHandler(this.frmBiblioteca_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView grlLibros;
-        private System.Windows.Forms.Button cmdConsultar;
         private System.Windows.Forms.Button cmdAtras;
         private System.Windows.Forms.Button cmdAdelante;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre_Libro;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo_Editorial;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo_Autor;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo_Distribuidor;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNombre_Libro;
+        private System.Windows.Forms.Label lblCodigo_Autor;
+        private System.Windows.Forms.Label lblNombre_Editorial;
+        private System.Windows.Forms.Label lblNombre_Distribuidor;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombreLibro;
+        private System.Windows.Forms.TextBox txtCodigoAutor;
+        private System.Windows.Forms.TextBox txtNombreEdit;
+        private System.Windows.Forms.TextBox txtNombreDistribuidor;
     }
 }
 
--- frmBiblioteca.cs
+++ frmBiblioteca.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace pryArlaEPRBioblioteca
 {
@@ -18,34 +19,142 @@ namespace pryArlaEPRBioblioteca
         {
             InitializeComponent();
         }
-        public string[,] matLibro = new string[20, 5];
+        public Datos[,] matLibros = new Datos [21, 5];
         public int contador = 0;
-        private void cmdConsultar_Click(object sender, EventArgs e)
+        public struct Datos
+        {
+            public int Codigo;
+            public string Nombre_del_Libro;
+            public string Nombre_Editorial;
+            public int  Codigo_Autor;
+            public string Nombre_Disitribuidor;
+        }
+
+        private void frmBiblioteca_Load(object sender, EventArgs e)
         {
 
             char separador = Convert.ToChar(",");
             int i = 0;
-        
+
             StreamReader srLibros = new StreamReader("./LIBRO.txt");
-            
-            while (!srLibros.EndOfStream && i < 21 )
+
+            while (!srLibros.EndOfStream && i < 21)
             {
-                string[] vecBiblioteca = srLibros.ReadLine().Split(separador);
+                string[] vecLibro = srLibros.ReadLine().Split(separador);
+                //borramos espacios en blanco
+                for (int indice = 0; indice < vecLibro.Length; indice++)
+                {
+                    vecLibro[indice] = Regex.Replace(vecLibro[indice], @"\t", "");
+                }
 
-                matLibro[i, 0] = vecBiblioteca[0];
-                matLibro[i, 1] = vecBiblioteca[1];
-                matLibro[i, 2] = vecBiblioteca[2];
-                matLibro[i, 3] = vecBiblioteca[3];
-                matLibro[i, 4] = vecBiblioteca[4];
+                matLibros[i, 0].Codigo = Convert.ToInt32(vecLibro[0]);
+                matLibros[i, 1].Nombre_del_Libro = vecLibro[1];
+                matLibros[i, 2].Nombre_Editorial = vecLibro[2];
+                matLibros[i, 3].Codigo_Autor = Convert.ToInt32(vecLibro[3]);
+                matLibros[i, 4].Nombre_Disitribuidor = vecLibro[4];
 
-                //StreamReader srEditorial = new StreamReader("./EDITORIAL.txt");
-                //while (!srEditorial.EndOfStream)
-                //{
-                //    string[] vecEditorial = srEditorial.ReadLine().Split(separador);
-                    
-                //}
+                //Asignamos los nombres de la editorial por nombre y no por numero 
+
+                StreamReader srEditorial = new StreamReader("./EDITORIAL.txt");
+
+                while (!srEditorial.EndOfStream )
+                {
+                    string[] vecEditorial = srEditorial.ReadLine().Split(separador);
+
+                    //Borramos espacio en blanco
+                    for (int indice = 0; indice < vecEditorial.Length; indice++)
+                    {
+                        //estamos remplazando una expresion por una cadena 
+                        vecEditorial[indice] = Regex.Replace(vecEditorial[indice], @"\t", "");
+                    }
+                    //Asignamos nombre 
+                    if (vecEditorial[0] == matLibros[i, 2].Nombre_Editorial)
+                    {
+                        matLibros[i, 2].Nombre_Editorial = vecEditorial[1];
+                        
+                    }
+                }
+                srEditorial.Close();
+
+                StreamReader srDistribuidora = new StreamReader("./DISTRIBUIDORA.txt");
+
+                while (!srDistribuidora.EndOfStream  )
+                {
+                    string[] vecDistribuidora = srDistribuidora.ReadLine().Split(separador);
+
+                    //Borramos espacio en blanco
+                    for (int indice = 0; indice < vecDistribuidora.Length; indice++)
+                    {
+                        //estamos remplazando una expresion por una cadena 
+                        vecDistribuidora[indice] = Regex.Replace(vecDistribuidora[indice], @"\t", "");
+                    }
+                    //Asignamos nombre 
+                    if (vecDistribuidora[0] == matLibros[i, 4].Nombre_Disitribuidor)
+                    {
+                        matLibros[i, 4].Nombre_Disitribuidor = vecDistribuidora[1];
+                       
+                    }
+                }
+                srDistribuidora.Close();
+
+                i++;
+
             }
             srLibros.Close();
+
+            txtCodigo.Text = Convert.ToString(matLibros[0, 0].Codigo);
+            txtNombreLibro.Text = matLibros[0, 1].Nombre_del_Libro;
+            txtNombreEdit.Text = matLibros[0, 2].Nombre_Editorial;
+            txtCodigoAutor.Text = Convert.ToString(matLibros[0, 3].Codigo_Autor);
+            txtNombreDistribuidor.Text = matLibros[0, 4].Nombre_Disitribuidor;
+
+            cmdAtras.Enabled = false;
+
+
+        }
+
+        private void cmdAdelante_Click(object sender, EventArgs e)
+        {
+            contador++;
+
+
+            txtCodigo.Text = Convert.ToString(matLibros[contador, 0].Codigo);
+            txtNombreLibro.Text = matLibros[contador, 1].Nombre_del_Libro;
+            txtNombreEdit.Text = matLibros[contador, 2].Nombre_Editorial;
+            txtCodigoAutor.Text = Convert.ToString(matLibros[contador, 3].Codigo_Autor);
+            txtNombreDistribuidor.Text = matLibros[contador, 4].Nombre_Disitribuidor;
+
+            cmdAdelante.Enabled = true;
+            cmdAtras.Enabled = true;
+
+            if (contador == matLibros.GetLength(0) - 1)
+            {
+                cmdAdelante.Enabled = false;
+            }
+        }
+
+        private void cmdAtras_Click(object sender, EventArgs e)
+        {
+            contador--;
+
+            if (contador >= 0)
+            {
+                txtCodigo.Text = Convert.ToString(matLibros[contador, 0].Codigo);
+                txtNombreLibro.Text = matLibros[contador, 1].Nombre_del_Libro;
+                txtNombreEdit.Text = matLibros[contador, 2].Nombre_Editorial;
+                txtCodigoAutor.Text = Convert.ToString(matLibros[contador, 3].Codigo_Autor);
+                txtNombreDistribuidor.Text = matLibros[contador, 4].Nombre_Disitribuidor;
+                
+            }
+            else
+            {
+                cmdAtras.Enabled = false;
+            }
+
+            if (contador == 0)
+            {
+                cmdAtras.Enabled = false;
+            }
 
         }
     }
